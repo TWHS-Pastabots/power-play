cb1c8d19fcd6354b87de676f877f94fc0706653c
JoshCPierre <joshcpierre@gmail.com>
2022-09-21 16:37:58 -0500

    Spaghetti
    Spaghetti update

--- Team16910/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Spaghetti.java
+++ Team16910/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Spaghetti.java
@@ -2,15 +2,45 @@ package main.java.org.firstinspires.ftc.teamcode.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import main.java.org.firstinspires.ftc.teamcode.Hardware.SpaghettiHardware;
+
 
 @TeleOp(name = "Spaghetti")
 public class Spaghetti extends OpMode
 {
+    // Initialization
+    SpaghettiHardware hardware;
+    final double FAST_SPEED = .8;
+    final double SLOW_SPEED = .5;
+    double slowConstant = FAST_SPEED;
+    ElapsedTime armTime = null;
+    ElapsedTime buttonTime = null;
 
     public void init()
     {
+        // Initialize Hardware
+        hardware = new SpaghettiHardware();
+        hardware.init(hardwareMap);
+        buttonTime = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+    }
+
+    public void start()
+    {
+        telemetry.addData("Status", "Started");
+        telemetry.update();
+    }
+    public void loop()
+    {
+        drive();
 
     }
+
     private void drive()
     {
         // Mecanum drivecode
@@ -24,13 +54,52 @@ public class Spaghetti extends OpMode
         double rightRearPower = y + x - rx;
 
 
+        if (Math.abs(leftFrontPower) > 1 || Math.abs(leftRearPower) > 1 ||
+                Math.abs(rightFrontPower) > 1 || Math.abs(rightRearPower) > 1 )
+        {
+            // Find the largest power
+            double max;
+            max = Math.max(Math.abs(leftFrontPower), Math.abs(leftRearPower));
+            max = Math.max(Math.abs(rightFrontPower), max);
+            max = Math.max(Math.abs(rightRearPower), max);
 
+            // Divide everything by max (it's positive so we don't need to worry
+            // about signs)
+            leftFrontPower /= max;
+            leftRearPower /= max;
+            rightFrontPower /= max;
+            rightRearPower /= max;
+        }
 
+        if (gamepad1.dpad_up || gamepad1.dpad_right)
+        {
+            leftFrontPower = -1;
+            rightRearPower = -1;
+            rightFrontPower = 1;
+            leftRearPower = 1;
+        }
+        else if (gamepad1.dpad_down || gamepad1.dpad_left)
+        {
+            leftFrontPower = 1;
+            rightRearPower = 1;
+            rightFrontPower = -1;
+            leftRearPower = -1;
+        }
 
-    }
+        if (gamepad1.square && slowConstant == FAST_SPEED && buttonTime.time() >= 500)
+        {
+            slowConstant = SLOW_SPEED;
+            buttonTime.reset();
+        }
+        else if (gamepad1.square && slowConstant == SLOW_SPEED && buttonTime.time() >= 500)
+        {
+            slowConstant = FAST_SPEED;
+            buttonTime.reset();
+        }
 
-    public void loop()
-    {
-
+        hardware.leftFront.setPower(leftFrontPower * slowConstant);
+        hardware.leftRear.setPower(leftRearPower * slowConstant);
+        hardware.rightFront.setPower(rightFrontPower * slowConstant);
+        hardware.rightRear.setPower(rightRearPower * slowConstant);
     }
 }
