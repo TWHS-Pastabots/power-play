c0ae2a320797c7ca97e93bb1f2d57a1f95bb9aa3
Zaheer Sufi <zaheerjunaidsufi@gmail.com>
2022-09-28 10:28:25 -0700

    beginning of autonomous

---
+++ Team16911/src/main/java/org/firstinspires/ftc/team16911/autonomous/BlueHome.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.team16911.autonomous;
+
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name="BlueHome")
+public class BlueHome extends LinearOpMode {
+    private final Pose2d placeCone = new Pose2d();
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
