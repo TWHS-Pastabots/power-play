24d7149dadbaa34a77e9fc4dd9f539cfd1bdc75f
Zaheer Sufi <84055134+zaheersufi@users.noreply.github.com>
2022-10-12 16:27:19 -0500

    Update SampleTankDrive.java

--- Team16911/src/main/java/org/firstinspires/ftc/team16911/drive/SampleTankDrive.java
+++ Team16911/src/main/java/org/firstinspires/ftc/team16911/drive/SampleTankDrive.java
@@ -1,16 +1,16 @@
-package org.firstinspires.ftc.team16910.drive;
+package org.firstinspires.ftc.team16911.drive;
 
-import static org.firstinspires.ftc.team16910.drive.DriveConstants.MAX_ACCEL;
-import static org.firstinspires.ftc.team16910.drive.DriveConstants.MAX_ANG_ACCEL;
-import static org.firstinspires.ftc.team16910.drive.DriveConstants.MAX_ANG_VEL;
-import static org.firstinspires.ftc.team16910.drive.DriveConstants.MAX_VEL;
-import static org.firstinspires.ftc.team16910.drive.DriveConstants.MOTOR_VELO_PID;
-import static org.firstinspires.ftc.team16910.drive.DriveConstants.RUN_USING_ENCODER;
-import static org.firstinspires.ftc.team16910.drive.DriveConstants.TRACK_WIDTH;
-import static org.firstinspires.ftc.team16910.drive.DriveConstants.encoderTicksToInches;
-import static org.firstinspires.ftc.team16910.drive.DriveConstants.kA;
-import static org.firstinspires.ftc.team16910.drive.DriveConstants.kStatic;
-import static org.firstinspires.ftc.team16910.drive.DriveConstants.kV;
+import static org.firstinspires.ftc.team16911.drive.DriveConstants.MAX_ACCEL;
+import static org.firstinspires.ftc.team16911.drive.DriveConstants.MAX_ANG_ACCEL;
+import static org.firstinspires.ftc.team16911.drive.DriveConstants.MAX_ANG_VEL;
+import static org.firstinspires.ftc.team16911.drive.DriveConstants.MAX_VEL;
+import static org.firstinspires.ftc.team16911.drive.DriveConstants.MOTOR_VELO_PID;
+import static org.firstinspires.ftc.team16911.drive.DriveConstants.RUN_USING_ENCODER;
+import static org.firstinspires.ftc.team16911.drive.DriveConstants.TRACK_WIDTH;
+import static org.firstinspires.ftc.team16911.drive.DriveConstants.encoderTicksToInches;
+import static org.firstinspires.ftc.team16911.drive.DriveConstants.kA;
+import static org.firstinspires.ftc.team16911.drive.DriveConstants.kStatic;
+import static org.firstinspires.ftc.team16911.drive.DriveConstants.kV;
 
 import androidx.annotation.NonNull;
 
@@ -38,10 +38,10 @@ import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
 
-import org.firstinspires.ftc.team16910.trajectorysequence.TrajectorySequence;
-import org.firstinspires.ftc.team16910.trajectorysequence.TrajectorySequenceBuilder;
-import org.firstinspires.ftc.team16910.trajectorysequence.TrajectorySequenceRunner;
-import org.firstinspires.ftc.team16910.util.LynxModuleUtil;
+import org.firstinspires.ftc.team16911.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.team16911.trajectorysequence.TrajectorySequenceBuilder;
+import org.firstinspires.ftc.team16911.trajectorysequence.TrajectorySequenceRunner;
+import org.firstinspires.ftc.team16911.util.LynxModuleUtil;
 
 import java.util.Arrays;
 import java.util.List;
