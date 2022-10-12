45873e97c995c7249864ce6abbe6620b8ee92ebd
Zaheer Sufi <84055134+zaheersufi@users.noreply.github.com>
2022-10-12 16:33:13 -0500

    Update ManualFeedforwardTuner.java

--- Team16911/src/main/java/org/firstinspires/ftc/team16911/drive/opmode/ManualFeedforwardTuner.java
+++ Team16911/src/main/java/org/firstinspires/ftc/team16911/drive/opmode/ManualFeedforwardTuner.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.team16910.drive.opmode;
+package org.firstinspires.ftc.team16911.drive.opmode;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
@@ -17,12 +17,12 @@ import org.firstinspires.ftc.team16910.drive.SampleMecanumDrive;
 
 import java.util.Objects;
 
-import static org.firstinspires.ftc.team16910.drive.DriveConstants.MAX_ACCEL;
-import static org.firstinspires.ftc.team16910.drive.DriveConstants.MAX_VEL;
-import static org.firstinspires.ftc.team16910.drive.DriveConstants.RUN_USING_ENCODER;
-import static org.firstinspires.ftc.team16910.drive.DriveConstants.kA;
-import static org.firstinspires.ftc.team16910.drive.DriveConstants.kStatic;
-import static org.firstinspires.ftc.team16910.drive.DriveConstants.kV;
+import static org.firstinspires.ftc.team16911.drive.DriveConstants.MAX_ACCEL;
+import static org.firstinspires.ftc.team16911.drive.DriveConstants.MAX_VEL;
+import static org.firstinspires.ftc.team16911.drive.DriveConstants.RUN_USING_ENCODER;
+import static org.firstinspires.ftc.team16911.drive.DriveConstants.kA;
+import static org.firstinspires.ftc.team16911.drive.DriveConstants.kStatic;
+import static org.firstinspires.ftc.team16911.drive.DriveConstants.kV;
 
 /*
  * This routine is designed to tune the open-loop feedforward coefficients. Although it may seem unnecessary,
