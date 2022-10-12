cbd79098a3c814981283f38e9ac0522c5a0028f4
Zaheer Sufi <84055134+zaheersufi@users.noreply.github.com>
2022-10-12 16:31:57 -0500

    Update TrajectorySegment.java

--- Team16911/src/main/java/org/firstinspires/ftc/team16911/trajectorysequence/sequencesegment/TrajectorySegment.java
+++ Team16911/src/main/java/org/firstinspires/ftc/team16911/trajectorysequence/sequencesegment/TrajectorySegment.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.team16910.trajectorysequence.sequencesegment;
+package org.firstinspires.ftc.team16911.trajectorysequence.sequencesegment;
 
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 
