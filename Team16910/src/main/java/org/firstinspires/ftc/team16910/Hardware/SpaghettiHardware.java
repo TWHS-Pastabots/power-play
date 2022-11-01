b876677089842a7813944e124054683ce567f59f
JoshCPierre <joshcpierre@gmail.com>
2022-11-01 17:37:27 -0500

    reverse claw servo
    daw

--- Team16910/src/main/java/org/firstinspires/ftc/team16910/Hardware/SpaghettiHardware.java
+++ Team16910/src/main/java/org/firstinspires/ftc/team16910/Hardware/SpaghettiHardware.java
@@ -90,13 +90,14 @@ public class SpaghettiHardware
         wheelServoL = hardwareMap.get(Servo.class, SpaghettiID.WHEEL_SERVOL);
         wheelServoR = hardwareMap.get(Servo.class, SpaghettiID.WHEEL_SERVOR);
 
-        servos = new Servo[]{clawServo, clawServo2, wheelServoL,wheelServoR};
+        servos = new Servo[]{clawServo, wheelServoL,wheelServoR};
 
         for(Servo servo : servos)
         {
             servo.setDirection(Servo.Direction.FORWARD);
             servo.setPosition(0);
         }
+        clawServo2.setDirection(Servo.Direction.REVERSE);
     }
 
 
